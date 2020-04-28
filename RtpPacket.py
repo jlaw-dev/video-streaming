cb69005376c14c34e09b710842fd5707ba0c4d0e
Kris Ables <krisables@gmail.com>
2020-04-28 14:29:11 -0500

    getSize
    Added getSize method

--- RtpPacket.py
+++ RtpPacket.py
@@ -85,3 +85,7 @@ class RtpPacket:
 	def getPacket(self):
 		"""Return RTP packet."""
 		return self.header + self.payload
+	
+	def getSize(self):
+		"""Return size of the payload and header"""
+		return len(self.header)+len(self.payload)
