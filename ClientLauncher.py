9e4e9da88f7aba94b3d35220069c3983ed40a94a
Jonathan Lawrence <Jonathan.Lawrence@utdallas.edu>
2020-04-30 21:59:58 -0500

    Created 3 button interface option

--- ClientLauncher.py
+++ ClientLauncher.py
@@ -9,13 +9,14 @@ if __name__ == "__main__":
                 serverPort = sys.argv[2]
                 rtpPort = sys.argv[3]
                 fileName = sys.argv[4]
+                has3ButtonInterface = bool(sys.argv[5])
         except:
-                print("[Usage: ClientLauncher.py Server_name Server_port RTP_port Video_file]\n")        
+                print("[Usage: ClientLauncher.py Server_name Server_port RTP_port Video_file Has_3_button_interface]\n")        
         
         root = Tk()
 
         # Create a new client
-        app = Client(root,serverAddr, serverPort,rtpPort,fileName)
+        app = Client(root,serverAddr,serverPort,rtpPort,fileName,has3ButtonInterface)
         app.master.title("RTPClient")   
         root.mainloop()
 	
